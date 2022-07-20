bb0ecfa23062c6d67f365eb4e784fb5c4ecea718
Tasha L <natasha.lewis.lalor@gmail.com>
2022-07-20 16:24:05 -0500

    Pre-bonus missions done

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
+  memberBeingEdited: object = null;
+
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
@@ -16,6 +18,24 @@ export class CrewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst})
+  }
+
+  remove(memberObj: object) {
+    let index = this.crew.indexOf(memberObj)
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
   }
 
 }

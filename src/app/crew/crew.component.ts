ce899c56fafc8854b2e13dd3320802adc6b35d3e
Tasha L <natasha.lewis.lalor@gmail.com>
2022-07-27 14:12:11 -0500

    pre-bonus crew component functional

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -24,6 +24,13 @@ export class CrewComponent implements OnInit {
 
   ngOnInit() { }
 
-  // Code the 'addCrewMember' function here:
+  addCrewMember(candidate) {
+    if(!this.crew.includes(candidate) && this.crew.length <= 2){
+      this.crew.push(candidate);
+    } else if (this.crew.includes(candidate)) {
+      this.crew.splice(this.crew.indexOf(candidate), 1)
+    }
+    
+  }
 
 }
